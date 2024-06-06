29b2209d81d6a9fe31637e6892655f2acc1aa2c6
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2024-06-06 20:30:01 +0100

    Time: 6 ms (99.86%) | Memory: 13.1 MB (22.19%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+          vector<char>st;
+        
+    
+        st.push_back(s[0]);
+        
+        for(int i=1;i<s.size();i++){
+               if(!st.empty() && st.back() == s[i]) {
+                   st.pop_back();
+               }else{
+                   st.push_back(s[i]);
+               }
+        }
+        
+        string ans(st.begin(),st.end());
+       
+        return ans;
+        
+    }
+};
