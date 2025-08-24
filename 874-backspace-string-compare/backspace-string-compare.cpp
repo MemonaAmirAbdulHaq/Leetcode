ad612866424eb49dcfd81ebb18e98390a9a76619
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2025-08-24 18:07:09 +0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (87.35%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        string result1 = "",result2 = "";
-        int i = 0;
-        
-        while(s[i]!='\0')
-        {
-            if(s[i]>='a' && s[i]<='z')
-                result1 += s[i];
-            else if(!result1.empty())
-                result1.pop_back();
-            ++i;                
+         int i = s.size() - 1;
+        int j = t.size() - 1;
+
+        while (i >= 0 || j >= 0) {
+            int skipS = 0;
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break; 
+                }
+            }
+
+            int skipT = 0;
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+           
+            if (i >= 0 && j >= 0) {
+                if (s[i] != t[j]) return false;
+            } else if (i >= 0 || j >= 0) {
+                return false;
+            }
+
+            i--;
+            j--;
         }
-        
-        i = 0;
-        while(t[i]!='\0')
-        {
-            if(t[i]>='a' && t[i]<='z')
-                result2 += t[i];
-            else if(!result2.empty())
-                result2.pop_back();
-            ++i;                
-        }
-        
-        if(result1.compare(result2)==0)
-            return true;
-        return false;
-        
+        return true;
     }
 };
